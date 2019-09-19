88e4eb5589800fcd03423ac299c52efbcd9b23e4
jess <jess50906@gmail.com>
2019-09-19 16:09:09 -0700

    started to add get date functions to js file

---
+++ time-card-system.js
@@ -0,0 +1,9 @@
+//employee time card system for 2 week pay period
+//each employee will have an id number
+//they will use their id number to clock in and clock out
+//each time they clock out, total worked hours for that pay 
+//period will be displayed
+
+//pull in current time and date
+var today = new Date();
+var date = today.getDate
